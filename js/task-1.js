307e3acef951f1b0d415cff99cb951f041f93d19
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-04-26 10:07:07 +0200

    task 1 file

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,7 @@ function isEnoughCapacity(products, containerSize) {
       totalProducts += products[productKey];
     }
   }
-  return totalProducts < containerSize;
+  return totalProducts <= containerSize;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
